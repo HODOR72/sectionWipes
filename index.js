4bbe511cd1cd9e874d02925fb27950c24fbf72d7
notlikethiss <notlikeethiss@gmail.com>
2024-02-07 22:41:15 +0300

    adaprite fix

--- index.js
+++ index.js
@@ -7,57 +7,57 @@ var wipeAnimation = new TimelineMax()
     "section.panel.turqoise",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   ) // in from left
   .to("section.panel .additionalContent", 1, {
     x: "-100%",
-    ease: Linear.easeNone,
+    ease: Linear.easeInOut,
   }) // scroll Content
   .fromTo(
     "section.panel.green",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   ) // in from right
   .fromTo(
     "section.panel.bordeaux",
     1,
     { y: "100%" },
-    { y: "0%", ease: Linear.easeNone }
+    { y: "0%", ease: Linear.easeInOut }
   ) // in from top
   .fromTo(
     "section.panel.pink",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   )
   .fromTo(
     "section.panel.aqua",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   )
   .fromTo(
     "section.panel.brown",
     1,
     { y: "100%" },
-    { y: "0%", ease: Linear.easeNone }
+    { y: "0%", ease: Linear.easeInOut }
   )
   .to("section.panel .additionalContent", 1, {
     x: "-100%",
-    ease: Linear.easeNone,
+    ease: Linear.easeInOut,
   }) // scroll Content
   .fromTo(
     "section.panel.bisque",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   )
   .fromTo(
     "section.panel.cadetblue",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
+    { x: "0%", ease: Linear.easeInOut }
   );
 
 // create scene to pin and link animation
