c848b011895967059392c2178f320e42dc93b584
notlikethiss <notlikeethiss@gmail.com>
2024-02-07 23:21:53 +0300

    fixing animation

--- index.js
+++ index.js
@@ -7,83 +7,66 @@ var wipeAnimation = new TimelineMax()
     "section.panel.turqoise",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   ) // in from left
   .to("section.panel .additionalContent", 1, {
     x: "-100%",
-    ease: Linear.easeInOut,
+    ease: Linear.easeNone,
   }) // scroll Content
   .fromTo(
     "section.panel.green",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   ) // in from right
   .fromTo(
     "section.panel.bordeaux",
     1,
     { y: "100%" },
-    { y: "0%", ease: Linear.easeInOut }
+    { y: "0%", ease: Linear.easeNone }
   ) // in from top
   .fromTo(
     "section.panel.pink",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   )
   .fromTo(
     "section.panel.aqua",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   )
   .fromTo(
     "section.panel.brown",
     1,
     { y: "100%" },
-    { y: "0%", ease: Linear.easeInOut }
+    { y: "0%", ease: Linear.easeNone }
   )
   .to("section.panel .additionalContent", 1, {
     x: "-100%",
-    ease: Linear.easeInOut,
+    ease: Linear.easeNone,
   }) // scroll Content
   .fromTo(
     "section.panel.bisque",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   )
   .fromTo(
     "section.panel.cadetblue",
     1,
     { x: "100%" },
-    { x: "0%", ease: Linear.easeInOut }
+    { x: "0%", ease: Linear.easeNone }
   );
 
-new ScrollMagic.Scene({
+var contentBlocks = document.querySelectorAll(".panel");
+
+var scene = new ScrollMagic.Scene({
   triggerElement: "#pinContainer",
-  triggerHook: 0,
-  duration: "400%",
+  triggerHook: "onLeave",
+  duration: "500%",
 })
   .setPin("#pinContainer")
   .setTween(wipeAnimation)
   .addTo(controller);
-
-// // Инициализация ScrollMagic
-// var controller = new ScrollMagic.Controller();
-
-// // Получаем все блоки контента
-// var contentBlocks = document.querySelectorAll(".panel");
-
-// // Проходимся по каждому блоку контента
-// contentBlocks.forEach(function (block) {
-//   // Создаем сцену ScrollMagic для каждого блока
-//   new ScrollMagic.Scene({
-//     triggerElement: "#pinContainer",
-//     triggerHook: "onLeave", // Начинать анимацию, когда верхняя граница блока достигнута верха окна
-//     duration: block.offsetHeight, // Длительность анимации - высота блока
-//   })
-//     .setPin("#pinContainer") // Фиксируем блок во время анимации
-//     .setTween(wipeAnimation)
-//     .addTo(controller);
-// });
