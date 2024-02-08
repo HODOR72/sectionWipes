d223e4920690c49dc93a9f2cdfe09c4ff1b8d9e3
Alexandr Arkhipov <72arkhipov@mail.ru>
2024-02-08 11:20:21 +0300

    refactor

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
-var controller = new ScrollMagic.Controller();
+const controller = new ScrollMagic.Controller()
 
 // define movement of panels
-var wipeAnimation = new TimelineMax()
-  .fromTo(
-    "section.panel.turqoise",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  ) // in from left
-  .to("section.panel .additionalContent", 1, {
-    x: "-100%",
-    ease: Linear.easeNone,
-  }) // scroll Content
-  .fromTo(
-    "section.panel.green",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  ) // in from right
-  .fromTo(
-    "section.panel.bordeaux",
-    1,
-    { y: "100%" },
-    { y: "0%", ease: Linear.easeNone }
-  ) // in from top
-  .fromTo(
-    "section.panel.pink",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  )
-  .fromTo(
-    "section.panel.aqua",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  )
-  .fromTo(
-    "section.panel.brown",
-    1,
-    { y: "100%" },
-    { y: "0%", ease: Linear.easeNone }
-  )
-  .to("section.panel .additionalContent", 1, {
-    x: "-100%",
-    ease: Linear.easeNone,
-  }) // scroll Content
-  .fromTo(
-    "section.panel.bisque",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  )
-  .fromTo(
-    "section.panel.cadetblue",
-    1,
-    { x: "100%" },
-    { x: "0%", ease: Linear.easeNone }
-  );
+const wipeAnimation = new TimelineMax()
+	.fromTo(
+		"section.panel.turqoise",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	) // in from left
+	.to("section.panel .additionalContent", 1, {
+		x: "-100%",
+		ease: Linear.easeNone,
+	}) // scroll Content
+	.fromTo(
+		"section.panel.green",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	) // in from right
+	.fromTo(
+		"section.panel.bordeaux",
+		1,
+		{ y: "100%" },
+		{ y: "0%", ease: Linear.easeNone }
+	) // in from top
+	.fromTo(
+		"section.panel.pink",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	)
+	.fromTo(
+		"section.panel.aqua",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	)
+	.fromTo(
+		"section.panel.brown",
+		1,
+		{ y: "100%" },
+		{ y: "0%", ease: Linear.easeNone }
+	)
+	.to("section.panel .additionalContent", 1, {
+		x: "-100%",
+		ease: Linear.easeNone,
+	}) // scroll Content
+	.fromTo(
+		"section.panel.bisque",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	)
+	.fromTo(
+		"section.panel.cadetblue",
+		1,
+		{ x: "100%" },
+		{ x: "0%", ease: Linear.easeNone }
+	)
 
-var contentBlocks = document.querySelectorAll(".panel");
+const contentBlocks = document.querySelectorAll(".panel")
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#pinContainer",
-  triggerHook: "onLeave",
-  duration: "500%",
+const scene = new ScrollMagic.Scene({
+	triggerElement: "#pinContainer",
+	triggerHook: "onLeave",
+	duration: "500%",
 })
-  .setPin("#pinContainer")
-  .setTween(wipeAnimation)
-  .addTo(controller);
+	.setPin("#pinContainer")
+	.setTween(wipeAnimation)
+	.addTo(controller)
 
-const tg = window.Telegram?.WebApp;
+const tg = window.Telegram?.WebApp
 if (tg) {
-  tg.ready();
-  tg.expand();
-  tg.enableConfimration();
+	tg.ready()
+	tg.expand()
+	tg.setBackgroundColor = "#efeef3"
+	tg.enableClosingConfirmation()
 }
